2a33795dd198140321b853c9e9ad50610fe605bc
EJohn <dev.ejohnpaguntalan@gmail.com>
2024-07-11 14:29:28 +0800

    Initial Commit

---
+++ MeSpace/MeSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeSpaceApp.swift
+//  MeSpace
+//
+//  Created by Ethan John Paguntalan on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MeSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
